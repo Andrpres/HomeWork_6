a8abb13d1fe37a6cf7e661a151e49f4ad2779723
Andrpre <andrpre96@gmail.com>
2022-12-10 12:45:06 +0300

    Finish 3 task

---
+++ Task3/Program.cs
@@ -0,0 +1,62 @@
+﻿/*
+Задача 45: Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+*/
+
+// Получение числа с консоли
+int GetNumberFromConsole(string message){
+    bool isCorrect = false;
+    int result = 0;
+
+    while (!isCorrect)
+    {
+        Console.WriteLine(message);
+
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            isCorrect = true;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число. Введите число");
+        }
+    }
+    return result;
+}
+
+//Добавляем все введенные числа в массив
+int[] Numbers(){
+    int[] numbers = new int[5];
+    for (int i = 0; i < 5; i++)
+    {
+        numbers[i] = GetNumberFromConsole("Введите число");
+    }
+    return numbers;
+}
+
+//метод, который будет печатать массив
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+//Копируем массив поэлементно
+int[] Copy(int[]array){
+    int[] copy = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        copy[i] = array[i];
+    }
+    return copy;
+}
+
+int[] resultArray = Numbers();
+Console.WriteLine($"Ваш массив: ");
+PrintArray(resultArray);
+Console.WriteLine();
+
+int[] resultCopy = Copy(resultArray);
+Console.WriteLine($"Копия вашего массива: ");
+PrintArray(resultCopy);
